ceed0eda17b163729aea6640fa2c0afca933d5ec
Alberto Sanmartin Martinez <albertosanmartinmartinez@gmail.com>
2018-03-03 19:30:21 +0100

    started

---
+++ JuegodeMemoria.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var numero = 100
+var i = 0
+
+while i < numero  {
+    //divisible entre 5
+    //es par
+    //impar
+    //rango entre 30 y 40
+    i += 1
+}
