dcae821104fe5f095b315cf7cfe3f28f8171aa1e
Alberto Sanmartin Martinez <albertosanmartinmartinez@gmail.com>
2018-03-04 10:48:46 +0100

    exercise done

--- JuegodeMemoria.playground/Contents.swift
+++ JuegodeMemoria.playground/Contents.swift
@@ -5,10 +5,22 @@ import UIKit
 var numero = 100
 var i = 0
 
-while i < numero  {
+for i in 0...100  {
+    print(i)
     //divisible entre 5
+    if  i%5 == 0 {
+        print("Es divisible por 5")
+    }
+    
     //es par
+    if  i%2 == 0 {
+        print("Es par")
+    }
+    
     //impar
+    if  i%2 != 0 {
+        print("Es impar")
+    }
+    
     //rango entre 30 y 40
-    i += 1
 }
